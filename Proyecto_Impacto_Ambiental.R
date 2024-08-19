4ce6073b36a24cf7bde16bb493e2e1888d059735
Nixon Rolando Garcia Ramirez <125457406+NixonGarciaRamirez@users.noreply.github.com>
2024-08-19 17:34:11 -0500

    Creacion de mapas tematicos

--- Proyecto_Impacto_Ambiental.R
+++ Proyecto_Impacto_Ambiental.R
@@ -29,3 +29,21 @@ areas_sensibles <- st_difference(uso_suelo2, areas_protegidas)
 
 # vixualizacion de areas sencibles
 plot(st_geometry(areas_sensibles), col = 'yellow', main = "Áreas Sensibles")
+
+
+#Crear Mapas Temáticos:
+library(ggplot2)
+library(tmap)
+
+# Ejemplo con ggplot2
+ggplot() +
+  geom_sf(data = uso_suelo2, fill = 'lightgreen') +
+  geom_sf(data = areas_protegidas, fill = NA, color = 'red') +
+  geom_sf(data = interseccion, fill = 'blue') +
+  labs(title = "Mapa de Uso del Suelo y Áreas Protegidas")
+
+# Ejemplo con tmap
+tmap_mode("view")
+tm_shape(uso_suelo) + tm_polygons("lightgreen") +
+  tm_shape(areas_protegidas) + tm_borders("red") +
+  tm_shape(interseccion) + tm_polygons("blue")
